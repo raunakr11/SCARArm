34b134da7e72cee54a7aaa4cb06239bc7dd02b60
Rajath-J-Relekar <79714276+Rajath-J-Relekar@users.noreply.github.com>
2023-11-06 13:35:14 +0530

    Z_mount_test.ino

--- Z_mount_test.ino
+++ Z_mount_test.ino
@@ -12,19 +12,19 @@ void loop() {
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 1200; x++) {
     digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(900);    // by changing this time delay between the steps we can change the rotation speed
+    delayMicroseconds(800);    // by changing this time delay between the steps we can change the rotation speed
     digitalWrite(stepPin,LOW); 
-    delayMicroseconds(900); 
+    delayMicroseconds(800); 
   }
    delay(1000); // One second delay
   
    digitalWrite(dirPin,LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
-   for(int x = 0; x < 1200; x++) {
+   for(int x = 0; x < 3600; x++) {
      digitalWrite(stepPin,HIGH);
-     delayMicroseconds(700);
+     delayMicroseconds(1000);
      digitalWrite(stepPin,LOW);
-     delayMicroseconds(700);
+     delayMicroseconds(1000);
    }
   delay(1000);
 }

a60a406a0e1311e949628cfceab1b9210e1c7f34
Raunak Rajput <78155138+raunakr11@users.noreply.github.com>
2023-11-19 17:18:48 +0530

    added limits of each motor

---
+++ Limits/Rotational_J1/Prismatic_J1/Prismatic_J1.ino
@@ -0,0 +1,41 @@
+// defines pins
+#define stepPin 2
+#define dirPin 5
+#define limitSwitch1 11
+ 
+//Z AXIS 
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(limitSwitch1, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+void loop() {
+  int limitval = digitalRead(limitSwitch1);
+  // Makes 200 pulses for making one full cycle rotation
+  if(digitalRead(limitSwitch1) != 1){
+    Serial.print("Going up  :  Switch is closed =   ");
+    Serial.println(limitval);
+    digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+    for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(800);    // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(800); } 
+  }
+  else if(digitalRead(limitSwitch1) ==1){
+    Serial.print("Going Down  :  Switch is open =  ");
+    Serial.println(limitval);
+    digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+    for(int x = 0; x < 14600; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(800);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(800);
+  }
+  delay(1000);
+  }
+}

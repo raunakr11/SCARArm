2a41266ea5e15c7f99ee9a167f8713a8b5867c7c
Raunak Rajput <78155138+raunakr11@users.noreply.github.com>
2023-11-22 14:22:43 +0530

    Update Homing_Accel_try3.ino

--- Homing/Homing_Accel_try3/Homing_Accel_try3.ino
+++ Homing/Homing_Accel_try3/Homing_Accel_try3.ino
@@ -1,89 +1,125 @@
-// #include <AccelStepper.h>
-
-// #define STEP_PIN 2
-// #define DIR_PIN 5
-// #define LIMIT_SWITCH_PIN 11
-
-// AccelStepper stepper(1, STEP_PIN, DIR_PIN);
-
-// void setup() {
-//   Serial.begin(9600);
-//   pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
-
-//   stepper.setMaxSpeed(3000);
-//   stepper.setAcceleration(1000);
-//   stepper.setSpeed(1000);
-
-//   int flag = 0;
-
-//   while(digitalRead(LIMIT_SWITCH_PIN) == LOW)
-//   {
-//     stepper.runSpeed(); // Run the stepper motor at the set speed
-//     if(digitalRead(LIMIT_SWITCH_PIN) == HIGH) break;
-//   }
-
-  
-//   // stepper.runSpeed(); // Run the stepper motor at the set speed
-
-//   // stepper.setSpeed(-1000);
-//   // stepper.move(100);
-
-//   // stepper.runSpeed(); // Run the stepper motor at the set speed
-//   // // stepper.runSpeedToPosition(false);
-
-// stepper.stop(); // Stop the motor
-
-//   // Change direction and move in the opposite direction
-//   stepper.setSpeed(-6000); // Set the speed in the opposite direction
-//   stepper.setAcceleration(1000);
-//   stepper.move(-2000); // Move 100 steps in the opposite direction
-
-//   // Run the motor until it reaches the desired position
-//   while (stepper.distanceToGo() != 0) {
-//     stepper.runSpeed();
-//   }
-  
-  
-// }
-
-// void loop() {
-// }
-
-
 #include <AccelStepper.h>
 
-#define STEP_PIN 2
-#define DIR_PIN 5
-#define LIMIT_SWITCH_PIN 11
+#define LIMIT_SWITCH_PIN3 11
+#define LIMIT_SWITCH_PIN1 10
+#define LIMIT_SWITCH_PIN2 9
+#define LIMIT_SWITCH_PIN4 A3
 
-AccelStepper stepper(1, STEP_PIN, DIR_PIN);
+AccelStepper stepperJ3(1, 2, 5);
+AccelStepper stepperJ1(1, 3, 6);
+AccelStepper stepperJ2(1, 4, 7);
+AccelStepper stepperJ4(1, 12, 13);
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
+void homing()
+{
+  homeJ3();
+  homeJ1();
+  homeJ2();
+  homeJ4();
+}
 
-  stepper.setMaxSpeed(5000);
-  stepper.setAcceleration(1000);
-  stepper.setSpeed(3000);
+void homeJ3()
+{
+  pinMode(LIMIT_SWITCH_PIN3, INPUT_PULLUP);
 
-  int flag = 0;
+  stepperJ3.setMaxSpeed(5000);
+  stepperJ3.setAcceleration(1000);
+  stepperJ3.setSpeed(1500);
 
-  while(digitalRead(LIMIT_SWITCH_PIN) == LOW) {
-    stepper.runSpeed(); // Run the stepper motor at the set speed
-    if(digitalRead(LIMIT_SWITCH_PIN) == HIGH) break;
+  while(digitalRead(LIMIT_SWITCH_PIN3) == LOW) {
+    stepperJ3.runSpeed();
+    if(digitalRead(LIMIT_SWITCH_PIN3) == HIGH) break;
   }
 
   // stepper.stop(); // Stop the motor
 
-  // Set speed and acceleration for the downward movement
-  stepper.setSpeed(-3000);
-  stepper.setAcceleration(1000);
-  stepper.move(-5000);
+  stepperJ3.setSpeed(-3000);
+  stepperJ3.move(-11000);
 
-  while (stepper.distanceToGo() != 0) {
-    Serial.println(stepper.distanceToGo());
-    stepper.runToPosition();
+  while (stepperJ3.distanceToGo() != 0) {
+    // Serial.println(stepper.distanceToGo());
+    stepperJ3.runToPosition();
   }
+  stepperJ3.
+}
+
+
+void homeJ1()
+{
+  pinMode(LIMIT_SWITCH_PIN1, INPUT_PULLUP);
+
+  stepperJ1.setMaxSpeed(5000);
+  stepperJ1.setAcceleration(1000);
+  stepperJ1.setSpeed(1500);
+
+  while(digitalRead(LIMIT_SWITCH_PIN1) == LOW) {
+    stepperJ1.runSpeed();
+    if(digitalRead(LIMIT_SWITCH_PIN1) == HIGH) break;
+  }
+
+  // stepper.stop(); // Stop the motor
+
+  stepperJ1.setSpeed(-3000);
+  stepperJ1.move(-8000);
+
+  while (stepperJ1.distanceToGo() != 0) {
+    // Serial.println(stepper.distanceToGo());
+    stepperJ1.runToPosition();
+  }
+}
+void homeJ2()
+{
+  pinMode(LIMIT_SWITCH_PIN2, INPUT_PULLUP);
+
+  stepperJ2.setMaxSpeed(5000);
+  stepperJ2.setAcceleration(1000);
+  stepperJ2.setSpeed(1500);
+
+  while(digitalRead(LIMIT_SWITCH_PIN2) == LOW) {
+    stepperJ2.runSpeed();
+    if(digitalRead(LIMIT_SWITCH_PIN2) == HIGH) break;
+  }
+
+  // stepper.stop(); // Stop the motor
+
+  stepperJ2.setSpeed(-3000);
+  stepperJ2.move(-5500);
+
+  while (stepperJ2.distanceToGo() != 0) {
+    // Serial.println(stepper.distanceToGo());
+    stepperJ2.runToPosition();
+  }
+}
+void homeJ4()
+{
+  pinMode(LIMIT_SWITCH_PIN4, INPUT_PULLUP);
+
+  stepperJ4.setMaxSpeed(5000);
+  stepperJ4.setAcceleration(1000);
+  stepperJ4.setSpeed(1500);
+
+  while(digitalRead(LIMIT_SWITCH_PIN4) == LOW) {
+    stepperJ4.runSpeed();
+    if(digitalRead(LIMIT_SWITCH_PIN4) == HIGH) break;
+  }
+
+  // stepper.stop(); // Stop the motor
+
+  stepperJ4.setSpeed(-3000);
+  stepperJ4.move(-1500);
+
+  while (stepperJ4.distanceToGo() != 0) {
+    // Serial.println(stepper.distanceToGo());
+    stepperJ4.runToPosition();
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+
+  homing();
+  
 }
 void loop() {
 
